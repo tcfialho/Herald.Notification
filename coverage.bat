17bcdccc1d1495bea4d0850079949c690afa9527
Thiago F <tcfialho@gmail.com>
2020-07-30 02:05:40 -0300

    chore: adding coverage support (#4)

--- coverage.bat
+++ coverage.bat
@@ -1,4 +1,4 @@
 dotnet tool install -g dotnet-reportgenerator-globaltool
-dotnet test /maxcpucount:1 /p:CollectCoverage=true /p:CoverletOutputFormat=\"json,opencover\" /p:CoverletOutput="../../coverage/" /p:MergeWith="../coverage.json" /p:Include="[Herald.Notification*.*]*"
-reportgenerator "-reports:../coverage/coverage.xml" "-targetdir:../coverage"
-start "" "..\coverage\index.htm"
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=\"json,opencover\" /p:CoverletOutput="../coverage/" /p:Exclude="[*.Test*.*]*"
+reportgenerator "-reports:coverage/coverage.opencover.xml" "-targetdir:coverage"
+start "" "coverage\index.htm"
